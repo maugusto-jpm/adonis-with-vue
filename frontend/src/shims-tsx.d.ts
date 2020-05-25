a87e7af5ec33e1686f6e41479857fb5d60473adf
Marcelo Nascimento <maugusto.jpm@gmail.com>
2020-05-25 06:11:24 -0300

    Setup eslint to frontend and backend

--- frontend/src/shims-tsx.d.ts
+++ frontend/src/shims-tsx.d.ts
@@ -1,11 +1,12 @@
-import Vue, { VNode } from "vue";
+// This file allows .tsx syntaxsupport in IDEs to write JSX-style typescript code
+
+import Vue, { VNode } from 'vue';
 
 declare global {
   namespace JSX {
-    // tslint:disable no-empty-interface
     interface Element extends VNode {}
-    // tslint:disable no-empty-interface
     interface ElementClass extends Vue {}
+
     interface IntrinsicElements {
       [elem: string]: any;
     }
--- frontend/src/shims-vue.d.ts
+++ frontend/src/shims-vue.d.ts
@@ -1,4 +1,7 @@
-declare module "*.vue" {
-  import Vue from "vue";
+// Helps Typescript and IDEs to understand .vue files
+
+declare module '*.vue' {
+  import Vue from 'vue';
+
   export default Vue;
 }
